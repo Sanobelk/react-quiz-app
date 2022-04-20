9e2a9b621a98626220d53c7163b7457a99282228
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-04-20 10:42:18 -0400

    Update Answers.js

--- src/Answers.js
+++ src/Answers.js
@@ -6,12 +6,6 @@ function Answers(props){
     return(
         <div className="answers-container">
             <button onClick={props.handleClick}>{props.answers[0]}</button>
-
-
-
-
-
-
             <button onClick={props.handleClick}>{props.answers[1]}</button>
             <button onClick={props.handleClick}>{props.answers[2]}</button>
             <button onClick={props.handleClick}>{props.answers[3]}</button>
