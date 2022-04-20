f56f3b54894a34ec5cf73746a0c9b0a1b2022dd4
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-04-20 10:41:42 -0400

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -15,11 +15,11 @@ function Content(props){
     function handleClick(event){
         let x = obj[count-1].answer[obj[count-1].correct]
             if(count < obj.length){
-            setCount(prev => prev+1);
-            setQuestionNum(obj[count].num);
-            setQuestion(obj[count].question);
-            setAnswers(obj[count].answer);
-            setCorrAns(obj[count].correct)
+                setCount(prev => prev+1);
+                setQuestionNum(obj[count].num);
+                setQuestion(obj[count].question);
+                setAnswers(obj[count].answer);
+                setCorrAns(obj[count].correct)
             }
             if(event.target.textContent == x ){
                 setNumberOfCorr(prev => prev+1)
@@ -42,22 +42,21 @@ function Content(props){
 
     return(
         <div className="content-container">
-            {!quizOver ? <div>
+            {!quizOver ? 
+        <div>
             <h2>QUESTION # {questionNum}</h2>
              <div>
                 <Question question={question}/>
                 <Answers answers={answers} handleClick={handleClick}/>
             </div>
-            </div>
+       </div>
             :
-            <div>
+       <div>
             <h1>You got {numberOfCorr} out of {obj.length}</h1>
             <button className="resetButton" onClick={reset}>Reset</button>
-            </div>
+       </div>
             }
-
-
-        </div>
+       </div>
     )
 }
 
